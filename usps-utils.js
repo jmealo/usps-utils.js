0ab7d8abf4dbcb0d4fdcb8130ea6fa0ac7160ba5
Jeffrey <jeffreymealo@gmail.com>
2013-02-25 21:33:01 -0500

    Update usps-utils.js
    Weird unicode insertion?

--- usps-utils.js
+++ usps-utils.js
@@ -17,7 +17,7 @@
 
 
     usps.state_abbr = [ 'AA','AE','AK','AL','AP','AR','AZ','CA','CO','CT','DC','DE','FL','GA','GU','HI',
-                        'IA','ID','IL','IN','KSåå','KY','LA','MA','MD','ME','MI','MN','MO','MS','MT','NC',
+                        'IA','ID','IL','IN','KS','KY','LA','MA','MD','ME','MI','MN','MO','MS','MT','NC',
                         'ND','NE','NH','NJ','NM','NV','NY','OH','OK','OR','PA','PR','RI','SC','SD','TN',
                         'TX','UT','VA','VT','WA','WI','WV','WY' ];
 
